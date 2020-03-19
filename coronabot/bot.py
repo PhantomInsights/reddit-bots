3c9a975be44541352581ee1366968314033882dd
Sebastián Osorio <svzosorio@gmail.com>
2020-03-18 18:14:37 -0600

    Update bot.py
    Remove semicolon

--- coronabot/bot.py
+++ coronabot/bot.py
@@ -150,7 +150,7 @@ def get_international_epidemiology():
         "Germany": "Alemania",
         "United Kingdom": "Reino Unido",
         "Switzerland": "Suiza",
-        "Colombia": "Colombia",
+        "Colombia": "Colombia"
     }
 
     url = "https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data"
